8ebbe636e9ac66bb6f1d2df8c638604ba156bf60
guohh13@tsinghua.edu.cn <guohh13@mails.tsinghua.edu.cn>
2017-03-03 18:20:35 +0800

    5. Longest Palindromic Substring

---
+++ 5. Longest Palindromic Substring/src/longest_palindromic_substring/Solution-Edition1.java
@@ -0,0 +1,31 @@
+package longest_palindromic_substring;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        ArrayList<Integer> startPosi = new ArrayList<Integer>();
+        int maxLength = 1;
+        int startPosition = 0, endPosition = 1;
+        for (int i = 0; i < s.length(); i ++) {
+            char c = s.charAt(i);
+            for (int j = 0; j < startPosi.size(); j ++) {
+                int sp = startPosi.get(j).intValue();
+                if ((sp - 1 >= 0) && (s.charAt(sp - 1) == c)) {
+                    startPosi.set(j, sp - 1);
+                    if (i - sp + 2 > maxLength) {
+                        maxLength = i - sp + 2;
+                        startPosition = sp - 1;
+                        endPosition = i + 1;
+                    }
+                } else {
+                    startPosi.remove(j);
+                    j = j - 1;
+                }
+            }
+            startPosi.add(i);
+            startPosi.add(i + 1);
+        }
+        return s.substring(startPosition, endPosition);
+    }
+}
--- 5. Longest Palindromic Substring/src/longest_palindromic_substring/Solution.java
+++ 5. Longest Palindromic Substring/src/longest_palindromic_substring/Solution.java
@@ -3,22 +3,33 @@ package longest_palindromic_substring;
 import java.util.ArrayList;
 
 public class Solution {
-	public static void main(String[] args) {
-        ArrayList<Integer> startPosi = new ArrayList<Integer>();
-//        int maxLength = 0;
-//        for (int i = 0; i < s.length(); i ++) {
-//            char c = s.charAt(i);
-//            for (Integer sp : startPosi) {
-//                if ((sp - 1 >= 0) && (s.charAt(sp - 1) == c)) {
-//                    sp
-//                } else {
-//                    startPosi.remove()
-//                }
-//            }
-//        }
-        startPosi.add(1);
-        Integer i = startPosi.get(0);
-        i = i - 1;
-        System.out.println(startPosi.get(0));
+    private int startPosi, endPosi, maxLength = 0;
+    public String longestPalindrome(String s) {
+        int maxLength = 0;
+        int length = s.length();
+        if (length < 2) {
+            return s;
+        }
+        for (int i = 0; i < length - 1; i ++) {
+            getMaxLength(s, i, i);
+            getMaxLength(s, i, i + 1);
+        }
+        return s.substring(startPosi, endPosi + 1);
+    }
+    
+    public void getMaxLength(String s, int stp, int edp) {
+        while ((stp >= 0) && (edp < s.length())) {
+            if (s.charAt(stp) == s.charAt(edp)) {
+                if (edp - stp + 1> maxLength) {
+                    startPosi = stp;
+                    endPosi = edp;
+                    maxLength = edp - stp + 1;
+                }
+                stp --;
+                edp ++;
+            } else {
+                break;
+            }
+        }
     }
 }
