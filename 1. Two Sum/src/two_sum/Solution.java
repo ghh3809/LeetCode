8278193273865a7bcab5a9031e422627d66be8d2
guohh13@tsinghua.edu.cn <guohh13@mails.tsinghua.edu.cn>
2017-03-03 14:46:48 +0800

    Q1:Two Sum

---
+++ 1. Two Sum/src/two_sum/Solution.java
@@ -0,0 +1,15 @@
+package two_sum;
+
+public class Solution {
+	public int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        for (int i = 0; i < length - 1; i ++) {
+            for (int j = i + 1; j < length; j ++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
