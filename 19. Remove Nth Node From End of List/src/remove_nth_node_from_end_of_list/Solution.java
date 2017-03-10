d6707b2f6513ff6e2dabc63689439d43f631b240
guohh13@tsinghua.edu.cn <guohh13@mails.tsinghua.edu.cn>
2017-03-10 17:46:08 +0800

    19. Remove Nth Node From End of List

---
+++ 19. Remove Nth Node From End of List/src/remove_nth_node_from_end_of_list/Solution.java
@@ -0,0 +1,26 @@
+package remove_nth_node_from_end_of_list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode front = head;
+        for (int i = 0; i < n; i ++) {
+            front = front.next;
+        }
+        if (front == null) return head.next;
+        ListNode back = head;
+        while (front.next != null) {
+            front = front.next;
+            back = back.next;
+        }
+        back.next = back.next.next;
+        return head;
+    }
+}
