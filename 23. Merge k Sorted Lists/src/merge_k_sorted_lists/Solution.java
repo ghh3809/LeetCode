5583c1fe050047900b615e4bcd8b6c1f3fbd2e09
guohh13@tsinghua.edu.cn <guohh13@mails.tsinghua.edu.cn>
2017-03-12 00:19:28 +0800

    23. Merge k Sorted Lists

---
+++ 23. Merge k Sorted Lists/src/merge_k_sorted_lists/Solution.java
@@ -0,0 +1,40 @@
+package merge_k_sorted_lists;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>() {
+            public int compare(ListNode node1, ListNode node2) {
+                if (node1.val > node2.val) {
+                    return 1;
+                } else if (node1.val < node2.val) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        ListNode root = new ListNode(0);
+        ListNode node = root;
+        for (ListNode n : lists) {
+            if (n != null) pq.add(n);
+        }
+        while (!pq.isEmpty()) {
+            ListNode n = pq.poll();
+            node.next = n;
+            node = node.next;
+            if (n.next != null) {
+                pq.add(n.next);
+            }
+        }
+        return root.next;
+    }
+}
